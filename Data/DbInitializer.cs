b9083a866acfffa312ac59aa42b458fafb129d83
ethawn <ethawn234@gmail.com>
2025-04-28 12:09:02 -0500

    refactor seed logic trigger from any Pizzas exist to maxId exists

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -9,46 +9,51 @@ namespace ContosoPizza.Data
     // The DbInitializer class and Initialize method are both defined as static.
     public static async Task Initialize(PizzaContext context) // Initialize accepts a PizzaContext object as a parameter.
     {
+      var maxId = await context.Pizzas.MaxAsync(p => (int?)p.Id);
       // If there are no records in any of the three tables, Pizza, Sauce, and Topping objects are created.
-      if (await context.Pizzas.AnyAsync()) return;
+      if (maxId == null)
+      {
+        // create Toppings
+        var pepperoniTopping = new Topping { Name = "Pepperoni", Calories = 130 };
+        var sausageTopping = new Topping { Name = "Sausage", Calories = 100 };
+        var hamTopping = new Topping { Name = "Ham", Calories = 70 };
+        var chickenTopping = new Topping { Name = "Chicken", Calories = 50 };
+        var pineappleTopping = new Topping { Name = "Pineapple", Calories = 75 };
 
-      // create Toppings
-      var pepperoniTopping = new Topping { Name = "Pepperoni", Calories = 130 };
-      var sausageTopping = new Topping { Name = "Sausage", Calories = 100 };
-      var hamTopping = new Topping { Name = "Ham", Calories = 70 };
-      var chickenTopping = new Topping { Name = "Chicken", Calories = 50 };
-      var pineappleTopping = new Topping { Name = "Pineapple", Calories = 75 };
+        // create Sauces
+        var tomatoSauce = new Sauce { Name = "Tomato", IsVegan = true };
+        var pestoSauce = new Sauce { Name = "Pesto", IsVegan = false };
 
-      // create Sauces
-      var tomatoSauce = new Sauce { Name = "Tomato", IsVegan = true };
-      var pestoSauce = new Sauce { Name = "Pesto", IsVegan = false };
+        // create Pizzas
+        var pizzas = new Pizza[]{
+            new() {
+              Name = "Pepperoni Delight",
+              Sauce = tomatoSauce,
+              Toppings = [pepperoniTopping]
+            },
+            new Pizza {
+              Name = "Meat Lovers",
+              Sauce = tomatoSauce,
+              Toppings = [pepperoniTopping, sausageTopping, hamTopping, chickenTopping]
+            },
+            new Pizza {
+              Name = "Pineapple Paradise",
+              Sauce = pestoSauce,
+              Toppings = new List<Topping>
+                {
+                  pineappleTopping
+                }
+            }
+          };
 
-      // create Pizzas
-      var pizzas = new Pizza[]{
-          new() {
-            Name = "Pepperoni Delight",
-            Sauce = tomatoSauce,
-            Toppings = [pepperoniTopping]
-          },
-          new Pizza {
-            Name = "Meat Lovers",
-            Sauce = tomatoSauce,
-            Toppings = [pepperoniTopping, sausageTopping, hamTopping, chickenTopping]
-          },
-          new Pizza {
-            Name = "Pineapple Paradise",
-            Sauce = pestoSauce,
-            Toppings = new List<Topping>
-              {
-                pineappleTopping
-              }
-          }
-        };
-
-      // context.Pizzas.AddRange(pizzas); // The Pizza objects (and their Sauce and Topping navigation properties) are added to the object graph by using AddRange.
-      // context.SaveChanges(); // The object graph changes are committed to the database by using SaveChanges.
-      await context.Pizzas.AddRangeAsync(pizzas);
-      await context.SaveChangesAsync();
-    }
+        // context.Pizzas.AddRange(pizzas); // The Pizza objects (and their Sauce and Topping navigation properties) are added to the object graph by using AddRange.
+        // context.SaveChanges(); // The object graph changes are committed to the database by using SaveChanges.
+        await context.Pizzas.AddRangeAsync(pizzas);
+        await context.SaveChangesAsync();
+        maxId = await context.Pizzas.MaxAsync(p => (int?)p.Id); // refresh maxId after inserting
+      }
+      // reseed
+      await context.Database.ExecuteSqlRawAsync($"DBCC CHECKIDENT ('Pizzas', RESEED, {maxId ?? 0})");
+      }
   }
 }
